3852d5d98e36f9e5f44328ee942e6ba175a485b8
Kieran Potts <hello@kieranpotts.com>
2024-02-07 08:50:00 +0000

    Add some (rough) Docker functions

---
+++ dist/functions/posix/docker.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# ==============================================================================
+# Aliases for `cd`.
+# ==============================================================================
+
+# Build an image and immediately run a container in the background from it.
+# Requires a Dockerfile in the current directory.
+# Optionally pass a name for the image (else one will be automatically created).
+#
+# Reference: https://stackoverflow.com/questions/36075525/
+#
+runDockerFromFile() {
+  if [ -z "$1" ]; then
+    docker run --detach $(docker build -q .)
+  else
+    docker build --tag "$1" .
+    docker run --detach "$1"
+  fi
+}
+
+listDockerImages() {
+  docker image ls
+}
